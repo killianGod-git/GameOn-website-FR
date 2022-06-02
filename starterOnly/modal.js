06bd83c4bc9eb7ce3576d42c1a53c259a9e2013b
Killian Godmez <kgodmez@gmail.com>
2022-06-02 12:06:30 +0200

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,16 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+closeBtn.addEventListener("click", closeModal);
 // launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+  function launchModal() {
+      modalbg.style.display = "block";
+  }
+console.log(formData);
+    function closeModal(){
+  modalbg.style.display = "none";
 }
-
-
