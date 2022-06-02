fa1f5f45783e3b8f6311f49ca72588262ce7e282
Killian Godmez <kgodmez@gmail.com>
2022-06-02 12:23:51 +0200

    ajout commentaire issue 1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,16 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelector(".close");
+// création const sur l'élément .close
+const closeBtn = document.querySelector(".close"); 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-closeBtn.addEventListener("click", closeModal);
+
 // launch modal form
   function launchModal() {
-      modalbg.style.display = "block";
+    modalbg.style.display = "block";
   }
-console.log(formData);
-    function closeModal(){
-  modalbg.style.display = "none";
-}
+
+// close modal event
+closeBtn.addEventListener("click", closeModal);
+// close modal form
+  function closeModal(){
+    modalbg.style.display = "none";
+  }
